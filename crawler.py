92a7f930eb9e1053dbe184e81314b590d92d6b6a
012050 <beomjunchoe@naver.com>
2023-05-17 16:42:31 +0900

    main, crawler 파일에 주석 추가

--- crawler.py
+++ crawler.py
@@ -1,66 +1,95 @@
+# 크롤링을 위한 모듈
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+# 경로 설정을 위한 모듈
 import os
+
+# 딜레이를 위한 모듈
 import time
 
+# 함수 작동 중 오류가 발생하면 0을 반환하고, 오류가 없으면 리스트를 반환
 def book_list(id, pw, test=False):
+    # 전체 작동 시간 측정 시작부분
     start = time.time()
+
+    # try, except가 하나의 묶음
+    # try 문에서 오류가 발생하면 크롬 드라이버가 없다는 오류 메시지를 출력하고 0을 반환
     try:
+        # 크롬 드라이버 경로 설정 및 크롬 드라이버 실행
         driver_path = os.getcwd() + "\chromedriver.exe"
         driver = webdriver.Chrome(driver_path)
     except:
         print("크롬 드라이버가 없습니다. chromedriver.exe를 다운받아 현재 디렉토리에 넣어주세요.")
         return 0
     driver.maximize_window()
+
+    # 도서관 홈페이지 접속
     try:
         driver.get("https://lib.deu.ac.kr/")
     except:
         print("페이지가 응답이 없습니다.")
         return 0
-    login_bnts = driver.find_element(By.XPATH, "/html/body/form[1]/header/div/div[1]/ul/li[4]/a")
-    login_bnts.click()
+    
+    # XPATH를 이용해 로그인 버튼 클릭
+    driver.find_element(By.XPATH, "/html/body/form[1]/header/div/div[1]/ul/li[4]/a").click()
+
+    # 페이지 로딩을 위해 3초 대기 로딩이 끝나면 3초가 다 안 지나도 다음 코드로 넘어감
     driver.implicitly_wait(3)
+
+    # 로그인 창에서 아이디와 비밀번호를 입력하고 엔터를 누름
     inputs = driver.find_elements(By.CLASS_NAME, "form-control")
     inputs[0].send_keys(id)
     inputs[1].send_keys(pw, Keys.ENTER)
     
+    # 1초 대기
     time.sleep(1)
 
+    # 아이디와 비밀번호가 틀렸을 경우 오류 메시지가 뜨고 오류 메시지를 확인하는 코드
     try:
-
-        # webdriver(driver, 10).until(EC.alert_is_present())
+        # 오류 메시지가 뜨면 오류 메시지를 확인하고 확인 버튼을 누름
         driver.switch_to.alert.accept()
-        # driver.switch_to.alert.dismiss()
         print("로그인 실패")
         driver.close()
         return 0
+    
+    # 오류 메시지가 뜨지 않으면 오류가 발생하지 않았다는 뜻이므로 pass
     except:
         print("로그인 성공")
         pass
 
-    driver.find_element(By.XPATH, "/html/body/form[1]/header/nav/div[1]/ul[2]/li[2]/a").click()
-    time.sleep(3)
-    try:
-        driver.get("https://lib.deu.ac.kr/lend_lend.mir")
-    except:
-        print("페이지가 응답이 없습니다.")
-        return 0
+    # 대출 기록 페이지로 이동
+    driver.get("https://lib.deu.ac.kr/lend_lend.mir")
+    driver.implicitly_wait(3)
 
+    # 새로고침 버튼 클릭
     try:
-        driver.implicitly_wait(3)
         research = driver.find_element(By.CLASS_NAME, "btn btn_mir_view btn-sm")
         research.click()
         print("대출 기록 새로고침 성공")
     except:
         print("대출 기록 새로고침 실패")
     
-    s_BookNameList = driver.find_elements(By.CLASS_NAME, "text-left")
+    # 도서 리스트가 있는 요소들을 찾고 리스트에 저장
+    SeleniumBookNameList = driver.find_elements(By.CLASS_NAME, "text-left")
     booknamelist = []
-    for a_book in s_BookNameList[1:]:
+    
+    # SeleniumBookNameList[0]은 header이므로 제외, SeleniumBookNameList[1]부터 책 이름이 들어있음
+    for a_book in SeleniumBookNameList[1:]:
         a_book = a_book.text.split('/')
         booknamelist.append(a_book)
+
+    # 크롬 드라이버 종료
     driver.close()
     
+    # 전체 작동 시간 측정 종료 및 출력
     print("크롤링 시간 {:.2f}초\n".format(time.time() - start))
+
+    # booknamelist가 비어있으면 대출 기록이 없다는 메시지를 출력하고 1을 반환
+    if len(booknamelist) == 0:
+        print("대출 기록이 없습니다.")
+        return 1
+    
+    # booknamelist가 비어있지 않으면 booknamelist를 반환
     return booknamelist
--- main.py
+++ main.py
@@ -1,46 +1,59 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect
 from crawler import book_list
 
+# 데이터베이스 + 더미 데이터
 db = { "student_number" : [20220001, 20221234], 
       "book_list" : {},
       "update_time" : {},
       }
 
+# 페이지 이름 설정
 app = Flask("Bookmate")
 
-
+# 메인 페이지(홈 페이지) 라우팅/ 리퀘스트 방법 GET, POST
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # 사용자에게 home.html 파일을 보여줌
     return render_template("home.html")
 
-
+# "기본 페이지 URL + /UserData" 라우팅
 @app.route("/UserData", methods=["GET", "POST"])
 def inputData():
     id, pw = "", ""
     if request.method == "POST" or request.method == "GET":
+        # 사용자가 입력한 데이터를 받아옴
         id = request.form.get("ID")
         pw = request.form.get("PASSWORD")
         id_len = len(id)
+
+    # 사용자가 입력한 데이터가 없거나, 학번이 8자리가 아니면 홈으로 리다이렉트
     if (id == "" or pw == "") or (id == None or pw == None) or (id_len != 8):
         return redirect("/")
     
+    # 사용자가 입력한 데이터를 데이터베이스에 저장하기 위한 형변환
     id, pw = int(id), str(pw)
 
+    # 데이터베이스에 학번에 있는지 확인
     for student_number_list in db["student_number"]:
         if id == student_number_list:
+            # 아이디가 일치하면 단순히 아이디와 비밀번호를 출력
             return f"일치 아이디: {id}, 비밀번호: {pw}, 길이: {id_len} 학번 리스트: {db['student_number']}"
         
+    # 데이터베이스에 학번이 없으면 데이터베이스에 학번 추가
     db["student_number"].append(id)
 
+    # 데이터베이스에 학번 추가 후, 해당 학번의 대출 리스트를 크롤링
     user_book_list = book_list(id=id, pw=pw)
+
+    # 크롤링에 실패하면 홈으로 리다이렉트(크롤러는 작동 중 오류가 발생하면 0을 리턴)
     if user_book_list == 0:
         print("웹 페이지 로딩 오류")
         return redirect("/")
+        # 현재는 작동하지 않는 코드, html 파일에 page_code를 넘겨주는 방식으로 변경해야 함(문제가 발생하면 홈으로 리다이렉트하고 알람을 띄우기 위함)
         return redirect("/", page_code=0)
     
-    # return f"불일치 아이디: {id}, 비밀번호: {pw}, 길이: {id_len} 학번 리스트: {db['student_number']}"
-    
-    return render_template("SearchResult.html", id=id)
+    # 코드가 정상적으로 작동하면 SearchResult.html 페이지에 책 리스트 출력
+    return render_template("SearchResult.html", id=user_book_list)
 
 
 # 이스터 에그, html 연습용
@@ -48,4 +61,5 @@ def inputData():
 def EarthAndMoon():
     return render_template("EarthAndMoon.html")
 
+# debug 모드로 실행
 app.run(debug=True)
