0cb872679580ca2df651a46ade7c6312a3ee999c
bsh2389 <132681604+bsh2389@users.noreply.github.com>
2023-05-23 00:32:07 +0900

    알고리즘 테스트 1
    일부 수정

--- algorithm_test.py
+++ algorithm_test.py
@@ -15,17 +15,16 @@ def recomand(code, test=False):
     except:
         print("페이지가 응답이 없습니다.")
         return 0
-
-    driver.find_element(By.XPATH, "/html/body/form[1]/header/div/div[2]/div/div/div/div/div/input").click()
+    # 페이지 최대화
+    driver.maximize_window()
+    
     # 페이지 로딩을 위해 3초 대기 로딩이 끝나면 3초가 다 안 지나도 다음 코드로 넘어감
     driver.implicitly_wait(3)
 
-    input_element = driver.find_element(By.CLASS_NAME, "form-control")
-    input_element.send_keys(code, Keys.ENTER)
+    driver.find_element(By.CLASS_NAME, "ui-autocomplete-input").send_keys(code, Keys.ENTER)
+    time.sleep(3)
 
-    driver.find_element(By.XPATH, "/html/body/form/section/section/section/div[2]/div[3]/form/div[1]/table/tbody/tr/td[2]/a").click()
-
-    time.sleep(1)
+    driver.find_element(By.CLASS_NAME, "book_title").click()
 
     driver.quit()
 
