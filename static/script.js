78c04cb8060c7e74b175814af9742e2ccebf744d
012050 <beomjunchoe@naver.com>
2023-05-27 02:51:46 +0900

    사용자 데이터 체크

---
+++ static/script.js
@@ -0,0 +1,21 @@
+function check_info() {
+    var id = document.getElementsByName("ID")[0].value;
+    var pw = document.getElementsByName("PASSWORD")[0].value;
+    // id 타입 체크하기
+    if (id == "" && pw == "") {
+        alert("아이디와 비밀번호를 입력하세요.");
+        // return false;
+    }
+    else if (id == ""){
+        alert("아이디를 입력하세요.");
+        // return false;
+    }
+    else if (pw == ""){
+        alert("비밀번호를 입력하세요.");
+        // return false;
+    }
+    else if (id.length != 8){
+        alert("학번을 아이디로 입력해주세요.")
+    }
+    // return true;
+}
