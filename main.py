80ad4f6246a32b9da80fa6edc509c4d7cdd82f88
012050 <beomjunchoe@naver.com>
2023-05-28 21:41:24 +0900

    DB 수정

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ from crawler import book_list
 from algorithm_test import recommand
 
 # 데이터베이스 + 더미 데이터
-db = { "student_number" : [20220001, 20221234], 
-      "book_list" : {},
-      }
+db = { "Students" : {
+    20220001 : ['오늘의 지구를 말씀드리겠습니다 :과학으로 읽는 지구 설명서', '(패러데이 ＆ 맥스웰) 공간에 펼쳐진 힘의 무대', '화학으로 이루어진 세상'],
+    20220002 : ['연어의 시간 :길 잃은 물고기와 지구, 인간에 관하여', '모서리를 걸어요 6 :창작이십일작가회 이천이십이년 작품집', '불놀이']},
+    }
 
 # 페이지 이름 설정
 app = Flask("Bookmate")
 
 # 메인 페이지(홈 페이지) 라우팅/ 리퀘스트 방법 GET, POST
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
     # 사용자에게 home.html 파일을 보여줌
-    return render_template("home.html")
+    return render_template("home_remake.html")
 
 # "기본 페이지 URL + /UserData" 라우팅
 @app.route("/UserData", methods=["GET", "POST"])
 def inputData():
     id, pw = "", ""
-    if request.method == "POST" or request.method == "GET":
+    if request.method == "POST":
         # 사용자가 입력한 데이터를 받아옴
         id = request.form.get("ID")
         pw = request.form.get("PASSWORD")
@@ -33,14 +34,12 @@ def inputData():
     id, pw = int(id), str(pw)
 
     # 데이터베이스에 학번에 있는지 확인
-    for student_number_list in db["student_number"]:
+    for student_number_list in db["Students"]:
         if id == student_number_list:
             # 아이디가 일치하면 단순히 아이디와 비밀번호를 출력
+            return f"일치<br>아이디: {id} 비밀번호: {pw}<br>도서 목록: {db['Students'][student_number_list]}<br>천체 DB: {db}"
             return render_template("SearchResult.html")
         
-    # 데이터베이스에 학번이 없으면 데이터베이스에 학번 추가
-    db["student_number"].append(id)
-
     # 데이터베이스에 학번 추가 후, 해당 학번의 대출 리스트를 크롤링
     user_book_list = book_list(id=id, pw=pw)
 
@@ -53,8 +52,12 @@ def inputData():
     
     elif user_book_list == 1:
         print("대출 기록이 없습니다.")
-        return redirect("/")
+    
+    # 데이터베이스에 학번이 없으면 데이터베이스에 학번 추가
+    db["Students"][id] = user_book_list
+
     # 코드가 정상적으로 작동하면 SearchResult.html 페이지에 책 리스트 출력
+    return f"아이디 비번을 추가합니다.<br>아이디: {id} 비밀번호: {pw}<br>도서 목록: {db['Students'][id]}<br>천체 DB: {db}"
     return render_template("SearchResult.html")
 
 
@@ -64,4 +67,4 @@ def EarthAndMoon():
     return render_template("EarthAndMoon.html")
 
 # debug 모드로 실행
-app.run(debug=True)
+app.run(debug=True, host="0.0.0.0")
