375ef6bdd4e5d2f313768ddc553391a356bcfed7
012050 <beomjunchoe@naver.com>
2023-05-30 18:47:05 +0900

    리턴 값 제거

--- static/script.js
+++ static/script.js
@@ -4,18 +4,14 @@ function check_info() {
     // id 타입 체크하기
     if (id == "" && pw == "") {
         alert("아이디와 비밀번호를 입력하세요.");
-        // return false;
     }
     else if (id == ""){
         alert("아이디를 입력하세요.");
-        // return false;
     }
     else if (pw == ""){
         alert("비밀번호를 입력하세요.");
-        // return false;
     }
     else if (id.length != 8){
         alert("학번을 아이디로 입력해주세요.")
     }
-    // return true;
 }
