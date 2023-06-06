27b2496822989860a82eebdba29f96092796381b
012050 <beomjunchoe@naver.com>
2023-06-06 23:11:49 +0900

    주석 추가

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,21 +2,22 @@ import hashlib
 import json
 import time
 import random
+# 데이터 암호화
 def sha512_hash(data):
     sha512_hash = hashlib.sha512(str(data).encode()).hexdigest()
     return sha512_hash
-
+# 데이터 형변환
 def listTostr(data):
     str_data = json.dumps(data)
     return str_data
-
+# 데이터 형변환
 def strTolist(data):
     list_data = json.loads(data)
     return list_data
-
+# 현재 시간 반환
 def now_time():
     return time.strftime("%Y-%m-%d", time.localtime(time.time()))
-
+# 테스트용 랜덤한 숫자 생성
 def generate_random_number(Number_):
     min_value = 10 ** (Number_ - 1)
     max_value = (10 ** Number_) - 1
